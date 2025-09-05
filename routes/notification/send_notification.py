8d09eac62c1e874c32323d475337b9a10737e00f
satyammalviya05 <satyammalviya48@gmail.com>
2025-09-05 15:29:50 +0530

    all notification

--- routes/notification/send_notification.py
+++ routes/notification/send_notification.py
@@ -13,6 +13,11 @@ class NotificationPayload(BaseModel):
     title: str
     message: str
 
+class NotificationPayloadAll(BaseModel):
+    user_id: str
+    title: str
+    message: str
+
 @router.post("/", status_code=status.HTTP_200_OK)
 async def send_notification(payload: NotificationPayload):
     """
@@ -36,3 +41,26 @@ async def send_notification(payload: NotificationPayload):
     #     title="New Task Assigned",
     #     message="Please follow up with lead #1234."
     # )
+
+
+@router.post("/all", status_code=status.HTTP_200_OK)
+async def send_notification(payload: NotificationPayloadAll):
+    """
+    Send a single notification to a connected user.
+    """
+    success = await notification_service.notify_all(
+        title= payload.title,
+        message= payload.message,
+    )
+    if not success:
+        # e.g. user not connected
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"User {payload.user_id} is not connected"
+        )
+    return {"success": True}
+
+
+
+
+
